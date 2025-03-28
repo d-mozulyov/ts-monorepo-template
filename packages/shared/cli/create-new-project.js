c63363f595cd635e45d6ee278b8d2fe0219b21cb
Dmitry Mozulyov <d.mozulyov@powertech.center>
2025-03-28 12:51:06 +0300

    Обновил скрипты. Теперь есть создание sharedModule

--- packages/shared/cli/create-new-project.js
+++ packages/shared/cli/create-new-project.js
@@ -3,16 +3,20 @@
  * This will be expanded in the future to support all project types
  */
 
+const chalk = require('chalk');
+
 /**
  * Creates a new project based on the selected template
+ * @param {string} rootDir - The root directory of the monorepo
  * @param {string} projectType - The type of project to create
  * @returns {Promise<void>}
  */
-async function createNewProject(projectType) {
+async function createNewProject(rootDir, projectType) {
   console.log(`Creating project of type: ${projectType}`);
+  console.log(`Monorepo root directory: ${rootDir}`);
   
   // This function will be expanded in the future to include actual project creation logic
-  // Currently it just logs the selected project type
+  // Currently it just logs the selected project type and root directory
   
   // TODO: Implement actual project creation based on projectType
 }
